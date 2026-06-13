a940b5901ecd05c3d5ea449aa756e3d3f26fc33f
nahin113 <nahinahmed113@gmail.com>
2026-06-13 19:51:19 +0600

    implemented api endpoints for get and modify company data so that admin in the frontend get a good UI experience

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ async function run() {
     const database = client.db("nexushire_db");
     const jobCollection = database.collection("jobs");
     const companyCollection = database.collection("companies");
-    const applicationsCollection = database.collection("applications")
-    const planCollection = database.collection("plans")
-    const subscriptionCollection = database.collection("subscriptions")
-    const usersCollection = database.collection("user")
+    const applicationsCollection = database.collection("applications");
+    const planCollection = database.collection("plans");
+    const subscriptionCollection = database.collection("subscriptions");
+    const usersCollection = database.collection("user");
 
     app.post("/api/jobs", async (req, res) => {
       const job = req.body;
@@ -56,21 +56,27 @@ async function run() {
       res.json(result);
     });
 
-    app.get("/api/jobs/:id", async (req,res)=> {
-      const id = req.params.id
+    app.get("/api/jobs/:id", async (req, res) => {
+      const id = req.params.id;
       const query = {
-        _id : new ObjectId(id)
-      }
-      const result = await jobCollection.findOne(query)
-      res.json(result)
-    })
+        _id: new ObjectId(id),
+      };
+      const result = await jobCollection.findOne(query);
+      res.json(result);
+    });
+
+    app.get("/api/companies", async (req, res) => {
+      const cursor = companyCollection.find();
+      const result = await cursor.toArray();
+      res.json(result);
+    });
 
     app.post("/api/companies", async (req, res) => {
       const company = req.body;
       const newCompany = {
         ...company,
-        createdAt : new Date()
-      }
+        createdAt: new Date(),
+      };
       const result = await companyCollection.insertOne(newCompany);
       res.json(result);
     });
@@ -83,62 +89,75 @@ async function run() {
       res.json(result);
     });
 
-    app.post('/api/applications', async (req,res) => {
-      const application = req.body
+    app.patch("/api/companies/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedCompany = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: updatedCompany.status,
+        },
+      };
+      const result = await companyCollection.updateOne(filter, updatedDoc);
+      res.json(result);
+    });
+
+    app.post("/api/applications", async (req, res) => {
+      const application = req.body;
       const newApplication = {
         ...application,
-        createdAt : new Date()
-      }
-      const result = await applicationsCollection.insertOne(newApplication)
-      res.json(result)
-    })
+        createdAt: new Date(),
+      };
+      const result = await applicationsCollection.insertOne(newApplication);
+      res.json(result);
+    });
 
-    app.get('/api/applications', async (req,res)=> {
-      const query = {}
-      if(req.query.applicantId) query.applicantId = req.query.applicantId
-      if(req.query.jobId) query.jobId = req.query.jobId
-      const cursor = applicationsCollection.find(query)
-      const result = await cursor.toArray()
-      res.json(result)
-    })
+    app.get("/api/applications", async (req, res) => {
+      const query = {};
+      if (req.query.applicantId) query.applicantId = req.query.applicantId;
+      if (req.query.jobId) query.jobId = req.query.jobId;
+      const cursor = applicationsCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
 
+    // admin can post plans , should add app.post api plan here
 
-    // admin can post plans , should add app.post api plan here 
+    app.get("/api/plans", async (req, res) => {
+      const query = {};
+      if (req.query.plan_id) query.plan_id = req.query.plan_id;
+      const result = await planCollection.findOne(query);
+      console.log(result);
+      res.json(result);
+    });
 
-
-    app.get('/api/plans', async (req,res)=> {
-      const query = {}
-      if(req.query.plan_id) query.plan_id = req.query.plan_id;
-      const result = await planCollection.findOne(query)
-      console.log(result)
-      res.json(result)
-    })  
-
-    app.post('/api/subscriptions',async(req,res)=>{
-      const data = req.body
+    app.post("/api/subscriptions", async (req, res) => {
+      const data = req.body;
       const subsInfo = {
         ...data,
-        createdAt : new Date()
-      }
-      const result = await subscriptionCollection.insertOne(subsInfo)
+        createdAt: new Date(),
+      };
+      const result = await subscriptionCollection.insertOne(subsInfo);
 
-      const filter = {email : data.email}
+      const filter = { email: data.email };
 
       const updateDocument = {
-        $set : {
-          plan : data.planId
-        }
-      }
+        $set: {
+          plan: data.planId,
+        },
+      };
 
-      const updateResult = await usersCollection.updateOne(filter,updateDocument)
+      const updateResult = await usersCollection.updateOne(
+        filter,
+        updateDocument
+      );
 
       const resultObj = {
-        result : result,
-        updateResult : updateResult
-      }
-      res.json(resultObj)
-    })
-
+        result: result,
+        updateResult: updateResult,
+      };
+      res.json(resultObj);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
